be2224941e3af2622dc09a4444dbed83d337e1b6
OliviaYang6 <155361696+OliviaYang6@users.noreply.github.com>
2024-11-25 06:35:19 +0800

    E显示视频

--- Assets/environment/E.cs
+++ Assets/environment/E.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class E : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject videoPanel; 
+    public VideoPlayer videoPlayer; 
 
-    // Update is called once per frame
+    private bool isVideoPlaying = false; 
+
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (isVideoPlaying)
+            {
+                CloseVideo();
+            }
+            else
+            {
+                OpenVideo();
+            }
+        }
+    }
+
+
+    void OpenVideo()
+    {
+        if (videoPanel != null && videoPlayer != null)
+        {
+            videoPanel.SetActive(true); 
+            videoPlayer.Play();
+            isVideoPlaying = true;
+        }
+    }
+
+   
+    void CloseVideo()
+    {
+        if (videoPanel != null && videoPlayer != null)
+        {
+            videoPlayer.Stop(); 
+            videoPanel.SetActive(false); 
+            isVideoPlaying = false; 
+        }
     }
 }
