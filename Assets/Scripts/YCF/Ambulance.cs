1cc14d919fca2d07d2263acb19871e3de3c01fa1
OliviaYang6 <155361696+OliviaYang6@users.noreply.github.com>
2024-11-25 22:50:02 +0800

    人

---
+++ Assets/Scripts/YCF/Ambulance.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ambulance : MonoBehaviour
+{
+    public Animator ambulanceAnimator;
+    public string animationName = "AmbulanceAnimation";
+
+    private bool animationStarted = false;
+
+    void Start()
+    {
+        if (ambulanceAnimator == null)
+        {
+            ambulanceAnimator = GetComponent<Animator>();
+        }
+    }
+
+    void Update()
+    {
+        if (ambulanceAnimator != null && ambulanceAnimator.GetCurrentAnimatorStateInfo(0).IsName(animationName))
+        {
+            if (!animationStarted)
+            {
+                animationStarted = true;
+                Invoke(nameof(HideNPCs), 1f); 
+            }
+        }
+    }
+
+    void HideNPCs()
+    {
+        GameObject[] npcs = GameObject.FindGameObjectsWithTag("NPC"); 
+
+        foreach (GameObject npc in npcs)
+        {
+            npc.SetActive(false); 
+        }
+    }
+}
